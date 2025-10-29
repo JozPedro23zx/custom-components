2b6574dde2c0611963f7a0bb2ca109239731ec38
JozPedro23zx <josepedro23zx@gmail.com>
2025-10-29 07:53:48 -0300

    update monetary input

--- src/app/home/form/monetary/page.jsx
+++ src/app/home/form/monetary/page.jsx
@@ -1,51 +1,32 @@
 "use client"
 
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
 export default function MoneyInput() {
   const [value, setValue] = useState("0,00");
-  const inputRef = useRef(null);
 
   const formatValue = (numbers) => {
-    const num = parseFloat(numbers || "0") / 100;
+    let onlyNums = numbers.replace(/\D/g, "");
+    const num = parseFloat(onlyNums || "0") / 100;
     return num.toLocaleString("pt-BR", {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
   };
 
-  const handleKeyDown = (e) => {
-    if (!/[0-9]/.test(e.key) && e.key !== "Backspace") {
-      e.preventDefault();
-      return;
-    }
-    let onlyNums = value.replace(/\D/g, "");
-
-    if (e.key === "Backspace") {
-      onlyNums = onlyNums.slice(0, -1);
-    } else {
-      onlyNums += e.key;
-    }
-
-    setValue(formatValue(onlyNums));
-    e.preventDefault();
-  };
-
-  const handleFocus = () => {
-    const input = inputRef.current;
-    input.setSelectionRange(input.value.length, input.value.length);
+  const handleChange = (e) => {
+    setValue(formatValue(e.target.value))
   };
 
   return (
     <div className="flex flex-col w-64">
-      <label className="mb-1 text-sm text-gray-100">Valor (R$){parseFloat(value.replace('.', '').replace(',', '.'))}</label>
+      <label className="mb-1 text-sm">Valor {value} (R$)</label>
       <input
-        ref={inputRef}
         type="text"
         inputMode="numeric"
-        value={value}
-        onKeyDown={handleKeyDown}
-        onFocus={handleFocus}
+        placeholder="0,00"
+        value={value || ''}
+        onChange={(e) => handleChange(e)}
         className="border rounded-lg p-2 text-right text-lg"
       />
     </div>
